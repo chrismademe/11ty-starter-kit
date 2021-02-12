982477459f2a082e4d9c25b65e621dfdccde961d
Chris Galbraith <chris@resknow.co.uk>
2021-02-12 15:39:02 +0000

    Update utilities

--- templates/_data/site.js
+++ templates/_data/site.js
@@ -5,6 +5,11 @@ module.exports = {
 	name: `11ty Starter`,
 
 	/**
+	 * Site URL, we use this in the Sitemap
+	 */
+	url: `http://localhost:8080`,
+
+	/**
 	 * Global stylesheets, added in the <head> of each page
 	 *
 	 * For page specific stylesheets, use the same keys in
@@ -19,5 +24,18 @@ module.exports = {
 	 * For page specific javascript, use the same keys in
 	 * frontmatter.
 	 */
-	scripts: []
+	scripts: [],
+
+	/**
+	 * Cookie Consent
+	 *
+	 * A small banner will appear in the bottom left of the
+	 * page letting the user know this site uses cookies
+	 * to provide the best experience.
+	 *
+	 * You can customise the text shown by setting a text property
+	 */
+	cookieConsent: {
+		show: true
+	}
 };
