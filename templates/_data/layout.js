b23a195759dcccce8360c3c6f8cea1409fed4726
Chris Galbraith <chris@resknow.co.uk>
2021-02-12 16:22:20 +0000

    Explain the default layout

--- templates/_data/layout.js
+++ templates/_data/layout.js
@@ -1,1 +1,8 @@
+/**
+ * Set the default layout so you don't need to
+ * set it on every template.
+ *
+ * If you don't want to wrap your content in a layout,
+ * set layout: false in the template's frontmatter
+ */
 module.exports = 'default';
